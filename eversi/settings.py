c3a9559ad35ccdd01f3d461c2af417b6a0779578
v-reyder <v.reyder@2gis.ru>
2015-02-23 17:24:30 +0600

    Fix database

--- eversi/settings.py
+++ eversi/settings.py
@@ -59,12 +59,13 @@ WSGI_APPLICATION = 'eversi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {}
-#DATABASES['default'] = dj_database_url.config()
-
+DATABASES['default'] = dj_database_url.config()
+"""
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
+"""
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
