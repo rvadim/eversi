0108f615323a5e2b73f98ab4cadc74e63160e630
v-reyder <v.reyder@2gis.ru>
2015-02-23 14:19:15 +0600

    Add settings.configure() to wsgi

--- eversi/wsgi.py
+++ eversi/wsgi.py
@@ -1,6 +1,9 @@
 #import os
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eversi.settings")
 
+from django.conf import settings
+settings.configure()
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
 application = Cling(get_wsgi_application())
