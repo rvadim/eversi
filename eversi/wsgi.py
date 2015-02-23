16882270b8b10a0525a57e3e7ed1ebaa96ea6c0f
v-reyder <v.reyder@2gis.ru>
2015-02-23 14:24:12 +0600

    Remove settings.configure()

--- eversi/wsgi.py
+++ eversi/wsgi.py
@@ -1,8 +1,6 @@
 #import os
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eversi.settings")
 
-from django.conf import settings
-settings.configure()
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
