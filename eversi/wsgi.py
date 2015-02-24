29e979b4a32823d868e75b46c26ee4380ddde5e1
v-reyder <v.reyder@2gis.ru>
2015-02-24 20:30:15 +0600

    Restore default wsgi.py

--- eversi/wsgi.py
+++ eversi/wsgi.py
@@ -1,7 +1,14 @@
-#import os
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eversi.settings")
+"""
+WSGI config for eversi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eversi.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
